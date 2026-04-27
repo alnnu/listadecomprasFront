0506949c2258bea002a52dda65fbf647dd9b8c2a
alnnu <luannf1020@gmail.com>
2026-04-27 09:51:48 -0300

    add: theme

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -10,7 +10,7 @@ function ThemeProvider({
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="white"
       enableSystem
       disableTransitionOnChange
       {...props}
